5ce2db11017f197d1d879d8353e45c776bb5c44f
Mohamed_Mansour_E <mohamed_hamada@siliconexpert.com>
2021-05-01 09:04:00 +0200

    latest

--- components/DetailesComponent.js
+++ components/DetailesComponent.js
@@ -12,30 +12,42 @@ import {
 } from "react-native";
 import AppBar from "./AppBar";
 const Separator = () => <View style={styles.separator} />;
-export default function DetailesComponent({history , location}){
+export default function DetailesComponent({ history, location }) {
   const [macAddress, setMacAddress] = useState("");
   const [ipAddress, setIpAddress] = useState("");
 
-  return  ( <SafeAreaView style={styles.container}>
-  <AppBar title={location.state.name} />
-  <View style={styles.todo}>
-    <TextInput
-      placeholder="Add a Mac/IP"
-      value={macAddress}
-      onChangeText={(value) => setTitle(value)}
-      style={styles.textbox}
-    />
-    <TextInput
-      placeholder="Add a Mac/IP"
-      value={ipAddress}
-      onChangeText={(value) => setTitle(value)}
-      style={styles.textbox}
-    />
-    <Button title="Add" color="#7F39FB" onPress={() => addMacIpItem()} />
-  </View>
-  <Separator />
-</SafeAreaView>
-);
+  return (
+    <SafeAreaView style={styles.container}>
+      <AppBar title={location.state.name} />
+      <View style={styles.todo}>
+        <TextInput
+          placeholder="Add a Mac/IP"
+          value={macAddress}
+          onChangeText={(value) => setTitle(value)}
+          style={styles.textbox}
+        />
+      </View>
+      <View style={styles.todo}>
+        <TextInput
+          placeholder="Add a Mac/IP"
+          value={ipAddress}
+          onChangeText={(value) => setTitle(value)}
+          style={styles.textbox}
+        />
+      </View>
+
+      <View style={{ alignItems: "center", flexDirection: "row", justifyContent:"center" }}>
+        <Button
+          title="Add"
+          color="#7F39FB"
+          style={styles.button}
+          onPress={() => addMacIpItem()}
+        />
+      </View>
+
+      <Separator />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -56,5 +68,14 @@ const styles = StyleSheet.create({
     padding: 10,
     margin: 10,
     width: "80%",
-  }
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+  button: {
+    width: "90%", // no matter what value I set here, their width doesn't changes
+    height: 20,
+  },
 });
